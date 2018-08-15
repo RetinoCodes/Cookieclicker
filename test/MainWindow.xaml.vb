daafb2ea066b70137b88ea60e30d6a9ef7f14a6c
Maximilian Grauvogl <maximilian.grauvogl@gmail.com>
2018-08-15 18:03:33 +0200

    Left Click Right Click

--- test/MainWindow.xaml.vb
+++ test/MainWindow.xaml.vb
@@ -1,5 +1,34 @@
 ﻿Class MainWindow
-    Private Sub Coockie_GotFocus(sender As Object, e As RoutedEventArgs) Handles Coockie.GotFocus
+    Dim count As Double
+    Dim leftButton As Boolean
+    Dim rightButton As Boolean
+    Private Sub Cookie_MouseLeftButtonUp(sender As Object, e As MouseButtonEventArgs) Handles Cookie.MouseLeftButtonUp
+        If leftButton = True Then
+            count += 1
+            cookie_counter.Content = count.ToString()
+        End If
+    End Sub
 
+    Private Sub Cookie_MouseRightButtonUp(sender As Object, e As MouseButtonEventArgs) Handles Cookie.MouseRightButtonUp
+        If rightButton = True Then
+            count += 1
+            cookie_counter.Content = count.ToString()
+        End If
+    End Sub
+
+    Private Sub right_click_Checked(sender As Object, e As RoutedEventArgs) Handles right_click.Checked
+        rightButton = True
+    End Sub
+
+    Private Sub right_click_Unchecked(sender As Object, e As RoutedEventArgs) Handles right_click.Unchecked
+        rightButton = False
+    End Sub
+
+    Private Sub left_click_Unchecked(sender As Object, e As RoutedEventArgs) Handles left_click.Unchecked
+        leftButton = False
+    End Sub
+
+    Private Sub left_click_Checked(sender As Object, e As RoutedEventArgs) Handles left_click.Checked
+        leftButton = True
     End Sub
 End Class
